c13d00791707980bd76029b0e6ec5d1622072988
Yadu Mohan M C <yadumohan.mc@soti.net>
2025-02-10 21:24:13 +0530

    Icons visibility configiruation added

---
+++ src/TailBlazor.Toast/Configuration/ToastOptions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TailBlazor.Toast.Configuration
+{
+    public class ToastOptions
+    {
+        public bool IncludeIcons { get; set; } = true; 
+    }
+}
--- src/TailBlazor.Toast/ServiceCollectionExtensions.cs
+++ src/TailBlazor.Toast/ServiceCollectionExtensions.cs
@@ -1,12 +1,19 @@
 using TailBlazor.Toast.Services;
 using Microsoft.Extensions.DependencyInjection;
+using System;
+using TailBlazor.Toast.Configuration;
 
 namespace TailBlazor.Toast
 {
     public static class ServiceCollectionExtensions
     {
-        public static IServiceCollection AddTailBlazorToast(this IServiceCollection services)
+        public static IServiceCollection AddTailBlazorToast(this IServiceCollection services,  Action<ToastOptions>? configureOptions = null )
         {
+            services.Configure<ToastOptions>(options =>
+            {
+                options.IncludeIcons = true;
+                configureOptions?.Invoke(options);
+            });
             return services.AddScoped<IToastService, ToastService>();
         }
     }
